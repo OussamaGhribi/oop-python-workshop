aedf6557221e578916c4d2da40c4bb70c7d3d730
OussamaGhribi <137878019+OussamaGhribi@users.noreply.github.com>
2024-09-05 16:08:49 +0200

    Update client.py
    adding some codes with fixing errors

--- client.py
+++ client.py
@@ -4,17 +4,29 @@ import psycopg2.extras
 # Client class manesta3mlouha ken bech njibou mel base 
 #add tsir b class okhra ok
 class Client: #Client / ClientInDb / BaseClient
-    def __init__(self, firstname, lastname , id = None):
-        assert firstname, "first name lezem ykoun mch none"
-        assert isinstance(lastname, str), "first name lezem ykoun int"
+    def __init__(self, firstname, lastname, balance , id = None):
+        assert firstname, "first name lezem ykoun mch none" # first name is required no matter the type
+        assert isinstance(lastname, str), "first name lezem ykoun int" # last name is required a string type only
         self.firstname = firstname
         self.lastname = lastname
         self.__id = id
+        self.__balance = balance
         print("Welcome")
 
+    #getter 
     @property
     def id(self):
         return self.__id
+
+    #also getter
+    @property
+    def balance(self):
+        return self.__balance
+    
+    @balance.setter
+    def balance(self, amount):
+        self.__balance = amount
+
 
     def add_user_in_db(self):
         conn = psycopg2.connect(dbname="bankapp", 
